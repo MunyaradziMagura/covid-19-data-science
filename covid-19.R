751641f3270a9ead2cbd133851b80e20d9e2847c
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-28 12:09:27 +0930

    bar chart

--- covid-19.R
+++ covid-19.R
@@ -12,6 +12,21 @@ data_frame[is.na(data_frame)] <- 0 # replace NA with 0
 # list of countries we are looking for
 countries <- c("Australia", "China", "India","New Zealand","Sweden","Ukraine","United Kingdom","United States")
 
+# total number of cases 
+plot_ly(
+  data = data_frame[data_frame$location %in% countries,],
+  x = ~location,
+  y = ~new_cases,
+  color = ~location,
+  type = "bar"
+) %>%layout(title= list(text = "Total numbers of COVID-19 cases",font = "Helvetica"), font=t, 
+            legend=list(title=list(text='Countries',font = "Helvetica")), 
+            xaxis = list(title = list(text ='Countries', font = "Helvetica")),
+            yaxis = list(title = list(text ='Number of Cases', font = "Helvetica")),
+            plot_bgcolor='#e5ecf6')
+
+
+# cases line graph 
 plot_ly(
   data = data_frame[data_frame$location %in% countries,],
   x = ~date, 
