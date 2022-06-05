6cc3c22a6c7c77c09e89da7118d1bde6b807a2bd
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-06-05 18:13:00 +0930

    rich, average, poor countries cardiovasc_death_rate v covid deaths
    4.	Covid-19 was declared as a pandemic. However, there might be another pandemic no one talks about – cardiovascular diseases. Study the distribution of cardiovascular diseases (variable “cardiovasc_death_rate”) overall and compare it to COVID-19 death rate. Repeat this comparison for three groups of “rich”, “average” and “poor” countries

--- covid-19.R
+++ covid-19.R
@@ -1,10 +1,11 @@
 #install.packages("lubridate")
+#install.packages("zoo")                              # Install & load zoo
+library("zoo")
 library("dplyr")
 library("ggplot2")
 library("plotly")
 library("tidyverse")
-
-
+library("stringr")
 library("lubridate")
 
 data_frame <- read.csv( "owid-covid-data.csv", header = TRUE, sep = ",")
@@ -19,13 +20,7 @@ plot_ly(
   y = ~new_cases,
   color = ~location,
   type = "bar"
-) %>%layout(title= list(text = "Total numbers of COVID-19 cases",font = "Helvetica"), font=t, 
-            legend=list(title=list(text='Countries',font = "Helvetica")), 
-            xaxis = list(title = list(text ='Countries', font = "Helvetica")),
-            yaxis = list(title = list(text ='Number of Cases', font = "Helvetica")),
-            plot_bgcolor='#e5ecf6')%>% add_bars(
-              width = ~1
-            )
+)
 
 
 # cases line graph 
@@ -45,20 +40,96 @@ plot_ly(
 
 
 # QUESTION 3
-countryGdp <- data_frame[data_frame$gdp_per_capita > 0,] %>% select(gdp_per_capita,new_cases_per_million,new_deaths_per_million, date ) 
-countryGdp$date <- strftime(countryGdp$date, "%m") # get month only
-countryGdp <- countryGdp %>% group_by(date,gdp_per_capita) %>% summarise(cases = sum(new_cases_per_million), deaths = sum(new_deaths_per_million))
+countryGdp <- data_frame[data_frame$gdp_per_capita > 0,] %>% select(gdp_per_capita,total_cases_per_million,total_deaths_per_million, date, cardiovasc_death_rate) 
+countryGdp$date <- strftime(countryGdp$date, "%Y-%m") # get month only
 
+countryGdpQuantile <- unname(quantile(countryGdp$gdp_per_capita)) # get quantile
+cat("Poor Country: ",countryGdpQuantile[1],"\nAverage Country: ",countryGdpQuantile[2], "-",countryGdpQuantile[3],"\nRich Country: ",countryGdpQuantile[5],"+")
 
-countryGdp <- distinct(countryGdp, gdp_per_capita, .keep_all = TRUE) # 
-countryGdpQuantile <-  unname(quantile(countryGdp$gdp_per_capita)) # get quantile
-countryGdpQuantile
+countryGdp$wealth <-  cut(countryGdp$gdp_per_capita, breaks=c(countryGdpQuantile[1],countryGdpQuantile[2],countryGdpQuantile[4],countryGdpQuantile[5]),labels=c("low", "average", "high"))
+countryGdp <- drop_na(countryGdp)
+
+# get cases by country wealth
+lowCases <- countryGdp[countryGdp$wealth == 'low',] %>% group_by(date) %>% summarise(lowCases = max(total_cases_per_million)) %>% ungroup()
+averageCases <- countryGdp[countryGdp$wealth == 'average',] %>% group_by(date) %>% summarise(averageCases = max(total_cases_per_million)) %>% ungroup()
+highCases <- countryGdp[countryGdp$wealth == 'high',] %>% group_by(date) %>% summarise(highCases = max(total_cases_per_million)) %>% ungroup()
+
+# join cases into one dataframe 
+countryWealth <- full_join(lowCases,averageCases)
+countryWealth <- full_join(countryWealth, highCases)
+
+# plot case wealth dataframe
+plot_ly(countryWealth, x = ~date, y = ~countryWealth$lowCases, type = 'scatter', mode = "lines+markers", name = 'low') %>% 
+  add_trace(y = ~averageCases, name = 'average', mode = 'lines+markers')%>% 
+  add_trace(y = ~countryWealth$highCases, name = 'high', mode = 'lines+markers')%>% layout(title= list(text = "covid cases per-country by wealth",font = "Helvetica"), font=t, 
+                                                                                           legend=list(title=list(text='country wealth',font = "Helvetica")), 
+                                                                                           xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                                                                           yaxis = list(title = list(text ='total cases per million', font = "Helvetica")),
+                                                                                           plot_bgcolor='#e5ecf6') 
+
+# get death by country wealth
+lowDeath <- countryGdp[countryGdp$wealth == 'low',] %>% group_by(date) %>% summarise(lowDeath = max(total_deaths_per_million),cardiovasc = max(cardiovasc_death_rate)) %>% mutate(deaths_per_hundred_K = lowDeath / 10) %>% ungroup()
+averageDeath <- countryGdp[countryGdp$wealth == 'average',] %>% group_by(date) %>% summarise(averageDeath = max(total_deaths_per_million),cardiovasc = max(cardiovasc_death_rate)) %>% mutate(deaths_per_hundred_K = averageDeath / 10) %>% ungroup()
+highDeath <- countryGdp[countryGdp$wealth == 'high',] %>% group_by(date) %>% summarise(highDeath = max(total_deaths_per_million),cardiovasc = max(cardiovasc_death_rate))%>% mutate(deaths_per_hundred_K = highDeath / 10) %>% ungroup()
+
+# join cases into one dataframe 
+countryWealthDeath <- full_join(lowDeath,averageDeath)
+countryWealthDeath <- full_join(countryWealthDeath, highDeath)
+
+# plot case wealth dataframe
+plot_ly(countryWealthDeath, x = ~date, y = ~lowDeath, type = 'scatter', mode = "lines+markers", name = 'low') %>% 
+  add_trace(y = ~averageDeath, name = 'average', mode = 'lines+markers') %>% 
+  add_trace(y = ~highDeath, name = 'high', mode = 'lines+markers')%>% layout(title= list(text = "covid deaths per-country by wealth",font = "Helvetica"), font=t, 
+                                                                             legend=list(title=list(text='country wealth',font = "Helvetica")), 
+                                                                             xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                                                             yaxis = list(title = list(text ='total cases per million', font = "Helvetica")),
+                                                                             plot_bgcolor='#e5ecf6') 
+
 
 
 # QUESTION 4
 
+plot_ly(lowDeath, x = ~date, y = ~lowDeath, type = 'scatter', mode = "lines+markers", name = 'Covid Death rate') %>% 
+  add_trace(y = ~cardiovasc, name = 'Cardiovasc Death rate', mode = 'lines+markers')%>% layout(title= list(text = "Low GDP countries: Cardiovasc Death rate",font = "Helvetica"), font=t, 
+                                                                                 legend=list(title=list(text='Death Type',font = "Helvetica")), 
+                                                                                 xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                                                                 yaxis = list(title = list(text ='deaths per 100k', font = "Helvetica")),
+                                                                                 plot_bgcolor='#e5ecf6')
+
+plot_ly(averageDeath, x = ~date, y = ~averageDeath, type = 'scatter', mode = "lines+markers", name = 'Covid Death rate') %>% 
+  add_trace(y = ~cardiovasc, name = 'Cardiovasc Death rate', mode = 'lines+markers')%>% layout(title= list(text = "Average GDP countries: Cardiovasc Death rate",font = "Helvetica"), font=t, 
+                                                                                          legend=list(title=list(text='Death Type',font = "Helvetica")), 
+                                                                                          xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                                                                          yaxis = list(title = list(text ='deaths per 100k', font = "Helvetica")),
+                                                                                          plot_bgcolor='#e5ecf6')
+
+plot_ly(highDeath, x = ~date, y = ~highDeath, type = 'scatter', mode = "lines+markers", name = 'Covid Death rate') %>% 
+  add_trace(y = ~cardiovasc, name = 'Cardiovasc Death rate', mode = 'lines+markers')%>% layout(title= list(text = "High GDP countries: Cardiovasc Death rate",font = "Helvetica"), font=t, 
+                                                                                          legend=list(title=list(text='Death Type',font = "Helvetica")), 
+                                                                                          xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                                                                          yaxis = list(title = list(text ='deaths per 100k', font = "Helvetica")),
+                                                                                          plot_bgcolor='#e5ecf6')
+
+#############################################################################################################################
 
 
+cardiovasc <- cardiovasc %>% group_by(date) %>% summarise(cardiovasc = max(cardiovasc_death_rate))
+# inner join to get cardiovasc_death_rate per year 
+
+
+
+
+lowCardiovasc <- semi_join(lowCardiovasc, cardiovasc_death_rate, 
+                            by = c("date" =  "date")) %>% group_by(date) 
+
+
+
+
+
+
+
+
+# QUESTION 5
 
 # compare gdp to daily covid scores
 
@@ -69,54 +140,28 @@ monthlyExcessMortality <- excessMortality %>% group_by(date) %>% summarise(exces
 
 #wow <- data_frame %>% group_by(date) %>% summarise(total_sales = sum(excess_mortality)) %>% arrange(desc(total_sales))
 
+vline <- function(x = 0, color = "lightgreen") {
+  list(
+    type = "line", 
+    y0 = 0, 
+    y1 = 1, 
+    yref = "paper",
+    x0 = x, 
+    x1 = x, 
+    line = list(color = color)
+  )
+}
+
 plot_ly(
   data = monthlyExcessMortality,
   x = ~date, 
   y = ~excess_mortality,
   color = ~excess_mortality,
   type = "scatter",
-  mode = "lines+markers")
-
- 
-
-
-
-
+  mode = "lines+markers")%>% layout(shapes = list(vline("2020-06")),title = list(text = "Excess Mortality Rates",font = "Helvetica"), 
+                                    legend=list(title=list(text='Excess Mortality',font = "Helvetica")), 
+                                    xaxis = list(title = list(text ='Date', font = "Helvetica")),
+                                    yaxis = list(title = list(text ='total cases per million', font = "Helvetica")),
+                                    plot_bgcolor='#e5ecf6')
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#split split countries 
-rich <- 0
-average <- 0
-poor <- 0
